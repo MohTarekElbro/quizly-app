a3eb6b07b909a50fcfd6386d1099b280afe6001e
MohTarek <hamodelbrolosy@gmail.com>
2020-05-18 17:09:47 +0200

    new mongose connection

--- scr/db-con/mongoose.js
+++ scr/db-con/mongoose.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-require('dotenv').config({path:'./configurations/dev.env'})
+require('dotenv').config({ path: './configurations/dev.env' })
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://quizlydb:Mm123456@cluster0-k5tpi.mongodb.net/test?retryWrites=true&w=majority",
-{useNewUrlParser: true, 
-    useUnifiedTopology:true,
-    useCreateIndex:true,
-   useFindAndModify:false}
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
 )
 const conn = mongoose.connection;

da4e13d1d9e8a1593f92b77ee07760971ae0af2f
MohTarek <hamodelbrolosy@gmail.com>
2020-05-18 16:41:02 +0200

    mongodb change uri

--- scr/db-con/mongoose.js
+++ scr/db-con/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config({path:'./configurations/dev.env'})
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Question-Generation-System",
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://quizlydb:Mm123456@cluster0-k5tpi.mongodb.net/test?retryWrites=true&w=majority",
 {useNewUrlParser: true, 
     useUnifiedTopology:true,
     useCreateIndex:true,
